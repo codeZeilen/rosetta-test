e588b94dd3a5fbc5a38ebbc42665782582b77604
patrick <hi@patrickrein.de>
2024-07-15 21:17:39 +0200

    First stab at function-based ports

--- ports.py
+++ ports.py
@@ -9,28 +9,31 @@ class Placeholder(object):
 def create_placeholder(name, parameters):
     return Placeholder(name, parameters)
 
+def ports_assert(value):
+    assert value
+
 class PortsSuite(object):
 
     # TODO: Consider simplyfing the macros to just add to a single list of annotations and extract the relevant parts afterwards (using functions written in ports)
     def __init__(self, file_name) -> None:
         self.lispy_env = lispy.Env(outer=lispy.global_env)
         self.initialize_ports_primitives()
-        self.initialize_macros()
+        self.initialize_ports()
         with open(file_name, "r") as file:
             self.suite = lispy.parse(file.read())
-        print(self.suite)
-        print("Expanding suite")
         self.suite = lispy.expand(self.suite, True)
-        print(self.suite)
         self.suite = lispy.eval(self.suite, self.lispy_env)
+        
+        self.suite_name, self.suite_version, self.sources, self.placeholders, self.capabilities, self.setup, self.tear_down = self.suite
 
     def initialize_ports_primitives(self):
         self.lispy_env.update({
-            "create-placeholder": create_placeholder
+            "create-placeholder": create_placeholder,
+            "assert": ports_assert
         })
 
-    def initialize_macros(self):
-        with open("macros.lispy", "r") as file:
+    def initialize_ports(self):
+        with open("ports.lispy", "r") as file:
             lispy.eval(lispy.parse(file.read()), self.lispy_env)
 
     def placeholder(self, name):
@@ -49,7 +52,10 @@ class PortsSuite(object):
         return decorator
     
     def run(self):
-        2 + 3
+        self.lispy_env.update({
+            "capabilities": self.capabilities
+        })
+        lispy.eval(lispy.parse("""(map capability-run capabilities)"""), self.lispy_env)
 
 def suite(file_name):
     return PortsSuite(file_name)
