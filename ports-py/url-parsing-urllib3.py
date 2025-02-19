997593acad617254c9b8484cfd83a07a131eb4d9
patrick <hi@patrickrein.de>
2025-02-19 10:08:24 +0100

    [PortsPy] Adds Python bridges for URL parsing

---
+++ ports-py/url-parsing-urllib3.py
@@ -0,0 +1,29 @@
+import ports
+import urllib3 
+
+suite = ports.suite("suites/url-parsing-RFC.ports")
+
+@suite.placeholder("url-parse")
+def url_parse(env, url_string):
+    try:
+        return urllib3.util.parse_url(url_string)
+    except ValueError as e:
+        return e
+
+@suite.placeholder("parse-error?")
+def parse_error(env, parse_result):
+    return isinstance(parse_result, Exception)
+
+@suite.placeholder("url-scheme")
+def url_scheme(env, parse_result: urllib3.util.Url):
+    return parse_result.scheme or ""
+
+@suite.placeholder("url-authority")
+def url_authority(env, parse_result: urllib3.util.Url):
+    return parse_result.authority or ""
+
+#
+# Running
+#
+
+suite.run()
