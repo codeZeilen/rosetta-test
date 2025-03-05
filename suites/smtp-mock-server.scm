a4251ee7e696abbd013c4ff3879cc00480313076
patrick <hi@patrickrein.de>
2025-03-05 11:13:00 +0100

    Adds command-specific response codes

--- suites/smtp-mock-server.scm
+++ suites/smtp-mock-server.scm
@@ -6,12 +6,13 @@
     (define (server-stopped? server) (list-ref server 3))
     (define (server-extensions server) (list-ref server 4))
     (define (server-response-codes server) (list-ref server 5))
-    (define (server-auths server) (list-ref server 6))
-    (define (server-requests server) (list-ref server 7))
-    (define (server-tls? server) (list-ref server 8))
-    (define (server-tls-started? server) (list-ref server 9)) ; Was a TLS connection started via STARTTLS?
-    (define (server-auth-proc server) (list-ref server 10))
-    (define (server-message-data server) (list-ref server 11))
+    (define (server-command-response-codes server) (list-ref server 6))
+    (define (server-auths server) (list-ref server 7))
+    (define (server-requests server) (list-ref server 8))
+    (define (server-tls? server) (list-ref server 9))
+    (define (server-tls-started? server) (list-ref server 10)) ; Was a TLS connection started via STARTTLS?
+    (define (server-auth-proc server) (list-ref server 11))
+    (define (server-message-data server) (list-ref server 12))
 
     (define (server-set-thread! server new-thread) 
         (list-set! server 1 new-thread))
@@ -21,20 +22,22 @@
         (list-set! server 4 new-extensions))
     (define (server-set-response-codes! server new-codes) 
         (list-set! server 5 new-codes))
+    (define (server-set-command-response-codes! server new-codes) 
+        (list-set! server 6 new-codes))
     (define (server-set-auths! server new-auths) 
-        (list-set! server 6 new-auths))
+        (list-set! server 7 new-auths))
     (define (server-set-requests! server new-requests) 
-        (list-set! server 7 new-requests))
+        (list-set! server 8 new-requests))
     (define (server-enable-tls server) 
-        (list-set! server 8 #t))
+        (list-set! server 9 #t))
     (define (server-disable-tls server) 
-        (list-set! server 8 #f))
+        (list-set! server 9 #f))
     (define (server-set-tls-started! server boolean) 
-        (list-set! server 9 boolean))
+        (list-set! server 10 boolean))
     (define (server-set-auth-proc! server proc)
-        (list-set! server 10 proc))
+        (list-set! server 11 proc))
     (define (server-set-message-data! server data)
-        (list-set! server 11 data))
+        (list-set! server 12 data))
 
     (define (server-extensions-with-auth server) 
         (if (empty? (server-auths server))
@@ -51,13 +54,35 @@
         (map 
             car
             (server-auths server)))
+
+    ; Mock response codes can be set in three different ways:
+    ; 1. By setting response code to be used indefinitely (server-set-response-code! server "250 ")
+    ; 2. By setting response code for a specific command indefinitely (server-set-command-response-code! server "EHLO" "250 ")
+    ; 3. By adding a response code to the list of response codes (server-add-response-code! server "250 "), 
+    ;     which will be used in order and removed from the list
     ; Sets response code to be used indefinitely
     (define (server-set-response-code! server new-code) 
         (server-set-response-codes! server new-code))
+    (define (server-set-command-response-code! server command new-code)
+        (let
+            ((response-pair (assq command (server-command-response-codes server))))
+            (if (not response-pair)
+                (server-set-command-response-codes! server (append (server-command-response-codes server) (list (cons command new-code))))
+                (list-set! response-pair 1 new-code))))
     (define (server-add-response-code! server new-code) 
         (server-set-response-codes! server (append (server-response-codes server) (list new-code))))
     (define (server-has-response-code? server)
         (not (empty? (server-response-codes server))))
+    (define (server-has-command-response-code? server command)
+        (or 
+            (list? (assq command (server-command-response-codes server)))
+            (server-has-response-code? server)))
+    (define (server-next-command-response-code server command)
+        (let
+            ((response-pair (assq command (server-command-response-codes server))))
+            (if (list? response-pair)
+                (cdr response-pair)
+                (server-next-response-code server))))
     (define (server-next-response-code server) 
         (let 
             ((response-codes (server-response-codes server)))
@@ -71,7 +96,7 @@
     (define (let-server-complete-handler)
         (thread-sleep! 0.1))
 
-    (define (create-server tls-enabled) (list 'server '() '() #f '() '() '() '() tls-enabled #f (lambda (credentials connection success failure) '()) ""))
+    (define (create-server tls-enabled) (list 'server '() '() #f '() '() '() '() '() tls-enabled #f (lambda (credentials connection success failure) '()) ""))
     (define (server-stop server) (list-set! server 3 #t))
     (define (server-start server)
         (let 
@@ -111,9 +136,9 @@
             (define (close-sockets) (begin 
                 (socket-close connection)
                 (socket-close (server-socket server))))
-            (define (write-prepared-response-code-else alternative-branch) 
-                (if (server-has-response-code? server) 
-                    (socket-write connection (string-append (server-next-response-code server) "\r\n")) 
+            (define (write-prepared-response-code-else command alternative-branch) 
+                (if (server-has-command-response-code? server command) 
+                    (socket-write connection (string-append (server-next-command-response-code server command) "\r\n")) 
                     (alternative-branch)))
             (define (handle request-string) (begin
                 ; Log the request
@@ -125,6 +150,7 @@
                     ((command (string-upcase (request-command request-string)))
                         (args (request-arguments request-string)))
                     (write-prepared-response-code-else
+                        command
                         (lambda ()
                             (cond 
                                 ((= command "EHLO") (ehlo (car args)))
@@ -168,11 +194,14 @@
                         (if (not (or (string-suffix? "\r\n.\r\n" message-data) (= ".\r\n" message-data)))
                             (receive-all-data))))
                 (write-prepared-response-code-else
+                    "DATA"
                     (lambda ()
                         (socket-write connection "354 End data with <CR><LF>.<CR><LF>\r\n")))
                 (receive-all-data)
-                (write-prepared-response-code-else (lambda () 
-                    (socket-write connection "250 2.0.0 OK\r\n"))))
+                (write-prepared-response-code-else 
+                    "DATA"
+                    (lambda () 
+                        (socket-write connection "250 2.0.0 OK\r\n"))))
             (define (starttls args)
                 (socket-write connection "220 Ready to start TLS\r\n")
                 (let 
