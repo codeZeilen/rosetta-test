ee81facd44a9a63d80ca4f4cebba1829a85eb867
patrick <hi@patrickrein.de>
2025-04-29 14:49:55 +0200

    [JS] Fixes a defect in the json RFC parsing suite

--- rosetta-test-js/json-rfc.js
+++ rosetta-test-js/json-rfc.js
@@ -6,7 +6,7 @@ const jsonSuite = suite("rosetta-test-suites/json-rfc.ros");
 
 jsonSuite.placeholder("list-json-test-files", (env) => {
     // List the files in the suites/json-rfc-fixtures directory ending with .json
-    const directory = path.join('suites', 'json-rfc-fixtures');
+    const directory = path.join('rosetta-test-suites', 'json-rfc-fixtures');
     
     try {
         return readdirSync(directory).filter(file => file.endsWith('.json'));
@@ -19,7 +19,7 @@ jsonSuite.placeholder("list-json-test-files", (env) => {
 
 jsonSuite.placeholder("file-contents", (env, fileName) => {
     try {
-        return readFileSync(path.join("suites", "json-rfc-fixtures", fileName), "utf8");
+        return readFileSync(path.join("rosetta-test-suites", "json-rfc-fixtures", fileName), "utf8");
     } catch (error) {
         console.error("Error reading file:", error);
         return "";
